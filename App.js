ccde79972e246da64340d299f61e82af2a93ed2f
iqbal <68240567+Byteblorg@users.noreply.github.com>
2023-10-09 22:01:48 +0800

    App1 part 1

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, FlatList, Text, Pressable, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./BlockRGB";
+
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+   
+  ]);
+
+
+ // generate random RGB color
+ const randomColor = () => {
+  const randomValue = () => Math.floor(Math.random() * 255);
+  return {
+    red: randomValue(),
+    green: randomValue(),
+    blue: randomValue(),
+    id: Date.now().toString(),
+  };
+};
+
+const addRandomColor = () => {
+  setColorArray([...colorArray, randomColor()]);
+};
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function resetColors(){
+
+    setColorArray([])
+  }
+
+  return (
+    <View style={styles.container}>
+    <View style={styles.buttonContainer}>
+    <Pressable
+          style={({ pressed }) => [
+            styles.button,
+            styles.button1,
+            pressed && styles.buttonPressed,
+          ]}
+          onPress={addRandomColor}
+        >
+          <Text style={styles.buttonText}>Add Random Color</Text>
+        </Pressable>
+        <Pressable
+          style={({ pressed }) => [
+            styles.button,
+            styles.button2,
+            pressed && styles.buttonPressed,
+          ]}
+          onPress={resetColors}
+        >
+          <Text style={styles.buttonText}>Burn it</Text>
+        </Pressable>
+    </View>
+
+    <FlatList
+      style={{ width: "100%" }}
+      data={colorArray}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  </View>
+  );
+
+
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Colour List" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1, // Use flex to arrange items vertically
+  },
+  buttonContainer: {
+    padding: 10,
+    width: "20%"
+
+  },
+  button: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button1: {
+    backgroundColor: "#007AFF",
+  },
+  button2: {
+    backgroundColor: "#FF3B30",
+  },
+  buttonPressed: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    color: "#fff",
   },
 });
